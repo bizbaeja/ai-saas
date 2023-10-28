ab514035522803ddeac28c7ca13ccde336e770e4
bizbaeja <bjgbjg112@gmail.com>
2023-10-28 17:44:05 +0900

    landing 수정

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -5,8 +5,9 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 const LandingPage =()=> {
     return (
-   <div>LandingPage(Unprotected)
-    <div>
+   <div className="mb-8 space-y-4 center">
+    <h1 className="text-2xl font-bold text-center">알잘딱에 오신것을 환영합니다!</h1>
+    <div className="text-center space-x-3">
         <Link href="/sign-in">
         <Button variant="default">
             Login
