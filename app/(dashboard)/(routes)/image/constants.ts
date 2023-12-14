33a5697ca549e0bbe5f27ae2351125be2ac9de49
bizbaeja <bjgbjg112@gmail.com>
2023-12-14 16:43:46 +0900

    axios error

--- app/(dashboard)/(routes)/image/constants.ts
+++ app/(dashboard)/(routes)/image/constants.ts
@@ -1,49 +1,51 @@
 import * as z from "zod";
 
 export const formSchema = z.object({
-    prompt: z.string().min(1,{
-        message:"이미지 프롬프트를 입력해주세요."
+    prompt: z.string().min(1, {
+        message: "이미지 프롬프트를 입력해주세요."
     }),
-    amount:z.string().min(1),
-    resolution:z.string().min(1),
+    amount: z.string().min(1, {
+        message: "Amount를 선택해주세요."
+    }),
+    resolution: z.string().min(1, {
+        message: "Resolution을 선택해주세요."
+    }),
 });
 
 export const amountOptions = [
-
     {
-        value:"1",
-        label:"1 Photo"
+        value: "1",
+        label: "1 Photo"
     },
     {
-        value:"2",
-        label:"2 Photo"
+        value: "2",
+        label: "2 Photo"
     },
     {
-        value:"3",
-        label:"3 Photo"
+        value: "3",
+        label: "3 Photo"
     },
     {
-        value:"4",
-        label:"4 Photo"
+        value: "4",
+        label: "4 Photo"
     },
     {
-        value:"5",
-        label:"5 Photo"
+        value: "5",
+        label: "5 Photo"
     },
 ];
 
 export const resolutionOptions = [
-{
-    value:"256x256",
-    label:"256x256"
-},
-{
-    value:"512x512",
-    label:"512x512"
-},
-{
-    value:"1024x1024",
-    label:"1024x1024"
-}
-
+    {
+        value: "256x256",
+        label: "256x256"
+    },
+    {
+        value: "512x512",
+        label: "512x512"
+    },
+    {
+        value: "1024x1024",
+        label: "1024x1024"
+    }
 ];
